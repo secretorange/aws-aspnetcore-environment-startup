c693443db1fd12ce5222502272e4db66a1302c7e
Lee Gunn <lee.gunn@secretorange.co.uk>
2018-04-07 07:24:15 +0100

    Initial checkin

---
+++ AWSBoot/Boot/AWSEnvironmentService.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Amazon.EC2;
+using Amazon.EC2.Model;
+using Amazon.SimpleSystemsManagement;
+using Amazon.SimpleSystemsManagement.Model;
+using Amazon.Util;
+
+namespace AWSBoot.Boot
+{
+    public class AWSEnvironmentService
+    {
+        private readonly IAmazonEC2 EC2;
+
+        private readonly IAmazonSimpleSystemsManagement SimpleSystemsManagement;
+
+        private static readonly object InstanceIdPadlock = new Object();
+
+        private static readonly SemaphoreSlim TagsPadlock = new SemaphoreSlim(1, 1);
+
+        private static string _InstanceId = null;
+
+        private static bool InstanceIdInitialised = false;
+
+        private IList<Amazon.EC2.Model.Tag> Tags = null;
+
+        public AWSEnvironmentService(IAmazonEC2 ec2, IAmazonSimpleSystemsManagement simpleSystemsManagement)
+        {
+            EC2 = ec2;
+
+            SimpleSystemsManagement = simpleSystemsManagement;
+        }
+
+        public static bool IsEC2Instance()
+        {
+            return !String.IsNullOrWhiteSpace(InstanceId());
+        }
+
+        public static string InstanceId()
+        {
+            if (!InstanceIdInitialised)
+            {
+                lock (InstanceIdPadlock)
+                {
+                    // Double check InstanceId as it might have been changed
+                    if (!InstanceIdInitialised)
+                    {
+                        _InstanceId = EC2InstanceMetadata.InstanceId;
+
+                        Console.WriteLine("InstanceId: " + _InstanceId);
+                    }
+                }
+            }
+
+            return _InstanceId;
+        }
+
+        public async Task<bool> HasTag(string key, string value)
+        {
+            return (await GetInstanceTags()).Any(t => String.Compare(t.Key, key, true) == 0 && String.Compare(t.Value, value, true) == 0);
+        }
+
+        public async Task<Amazon.EC2.Model.Tag> GetTag(string key)
+        {
+            return (await GetInstanceTags()).FirstOrDefault(t => String.Compare(t.Key, key, true) == 0);
+        }
+
+        public async Task<string> GetTagValue(string key)
+        {
+            var tag = (await GetInstanceTags()).FirstOrDefault(t => String.Compare(t.Key, key, true) == 0);
+
+            return tag != null ? tag.Value : null;
+        }
+
+        public async Task<IDictionary<string, string>> GetParameters(string path)
+        {
+            var request = new GetParametersByPathRequest()
+            {
+                Path = path,
+                WithDecryption = true,
+                Recursive = true
+            };
+
+            var response = await SimpleSystemsManagement.GetParametersByPathAsync(request);
+
+            return response.Parameters.ToDictionary(p => p.Name, p => p.Value);
+        }
+
+        public async Task<IList<Amazon.EC2.Model.Tag>> GetInstanceTags()
+        {
+            if (Tags == null)
+            {
+                await TagsPadlock.WaitAsync();
+                try
+                {
+                    // Double check
+                    if (Tags == null)
+                    {
+                        var id = InstanceId();
+
+                        if (id != null)
+                        {
+                            var request = new DescribeInstancesRequest();
+
+                            request.InstanceIds = new List<string>() { id };
+
+                            var response = await EC2.DescribeInstancesAsync(request);
+
+                            var instance = response.Reservations.SelectMany(r => r.Instances).FirstOrDefault();
+
+                            if (instance != null && instance.Tags != null)
+                            {
+                                Tags = instance.Tags;
+                            }
+                        }
+                    }
+                }
+                finally
+                {
+                    TagsPadlock.Release();
+                }
+            }
+
+            return Tags;
+        }
+    }
+}
---
+++ AWSBoot/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using AWSBoot.Boot;
+
+namespace AWSBoot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost().Run();
+        }
+
+
+        public static IWebHost BuildWebHost()
+        {
+            // ===================================
+            // Get the boot config from the server
+            // ===================================
+            var bootConfig = Task.Run(() => BootHelper.GetConfig()).Result;
+
+            var webHost = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    // !!! IMPORTANT !!!
+                    // Set the environment from boot config
+                    context.HostingEnvironment.EnvironmentName = bootConfig.Environment;
+
+                    config.AddJsonFile("appsettings.json", optional: true)
+                          .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true);
+
+                    // !!! IMPORTANT !!!
+                    // If there are any parameters from the server
+                    // then we'll use them to override anything in the JSON files
+                    config.AddInMemoryCollection(bootConfig.Parameters);
+                })
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            return webHost;
+        }
+    }
+}
